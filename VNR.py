dd3151f72bf269c64f2784e66f1b5404102371c2
Ashraful Islam <ashrafule44@gmail.com>
2025-07-27 17:47:02 +0600

    Variable Name Case

---
+++ VNR.py
@@ -0,0 +1,10 @@
+#           <<<<<<<<<<<Variable Name Rules>>>>>>>>>>>>> 
+
+#   Camel case
+
+myVariableName = "John"
+#   Pascel case
+MyVariableName = "John"
+
+#   Snake Case
+my_variable_name = "John"
