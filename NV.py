809a7711c7ab448834302b5527968ffca1bbc160
Ashraful Islam <ashrafule44@gmail.com>
2025-07-28 11:32:00 +0600

    Python & Random  Numbers

---
+++ NV.py
@@ -0,0 +1,16 @@
+# >>>>>>>>>>>>>   Normal Number Variable  <<<<<<<<<<<<<<<<<<<<
+
+
+a= 15
+b= 10
+c= 4
+
+print (int(a))
+print (float(b))
+print (complex(c))
+
+#   >>>>>>>>>>>  Random Nunber  <<<<<<<<<<<<<
+
+import random
+
+print(random.randrange (0,10))
