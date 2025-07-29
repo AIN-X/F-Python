33484413c8057795126ec89d6b70e3e79b9f409a
Ashraful Islam <ashrafule44@gmail.com>
2025-07-29 11:47:44 +0600

    Python - Slicing Strings

---
+++ ss.py
@@ -0,0 +1,21 @@
+# Python - Slicing Strings
+
+print('\n>>>>>>>>>>>>>>>>>>>>>>>>>> Slicing <<<<<<<<<<<<<<<<<<<<<<<<<\n')
+b = 'Naeem Islam '
+
+print(b[2:5])
+
+print('\n>>>>>>>>>>>>>>>>>>>>>>>>>> Slice From the Start <<<<<<<<<<<<<<<<<<<<<<<<<\n')
+
+b = 'Naeem Islam '
+print(b[:6])
+
+print('\n>>>>>>>>>>>>>>>>>>>>>>>>>> Slice To the End <<<<<<<<<<<<<<<<<<<<<<<<<\n')
+
+b = 'Naeem Islam '
+print(b[1:])
+
+print('\n>>>>>>>>>>>>>>>>>>>>>>>>>> Negative Indexing <<<<<<<<<<<<<<<<<<<<<<<<<\n')
+
+b = 'Naeem Islam '
+print(b[-5:-2])
