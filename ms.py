8bd4e77ad642e89de23f6949ed7b54bc941f07a3
Ashraful Islam <ashrafule44@gmail.com>
2025-07-29 12:31:52 +0600

    Python - Modify Strings

---
+++ ms.py
@@ -0,0 +1,28 @@
+#              Python - Modify Strings
+
+print('\n>>>>>>>>>>>>>>>>>>>>>>>>>> Upper Case <<<<<<<<<<<<<<<<<<<<<<<<<\n')
+
+a = 'Hello world'
+print(a.upper())
+
+print('\n>>>>>>>>>>>>>>>>>>>>>>>>>> Lower Case <<<<<<<<<<<<<<<<<<<<<<<<<\n')
+
+a = 'Hello world'
+print (a.lower())
+
+print('\n>>>>>>>>>>>>>>>>>>>>>>>>>> Remove Whitespace <<<<<<<<<<<<<<<<<<<<<<<<<\n')
+
+a = 'Hello world'
+print(a.strip())
+
+
+print('\n>>>>>>>>>>>>>>>>>>>>>>>>>> Replace String <<<<<<<<<<<<<<<<<<<<<<<<<\n')
+
+a = 'Hello world'
+print(a.replace("H", "J" ))
+
+
+print('\n>>>>>>>>>>>>>>>>>>>>>>>>>> Split String <<<<<<<<<<<<<<<<<<<<<<<<<\n')
+
+a = 'Hello world'
+print(a.split(","), "\n")
